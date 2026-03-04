fd32564bbf9fbd2cdfc6085ce929c95cf8226708
buttertoasted <sylvestrelikeearlgreyverymuch@gmail.com>
2026-03-04 22:57:07 +0700

    bubble

--- src/main/java/logic/entity/characters/rangedCharacters/Bubble.java
+++ src/main/java/logic/entity/characters/rangedCharacters/Bubble.java
@@ -1,9 +1,14 @@
 package logic.entity.characters.rangedCharacters;
 
+import component.scene2.Scene2;
+import logic.entity.AttackData;
+import logic.entity.BaseProjectileAttack;
+import logic.entity.characterClass.HybridClass;
 import logic.entity.characterClass.RangedClass;
 import logic.gameLogic.Player;
+import logic.interfaces.SpawnAttack;
 
-public class Bubble extends RangedClass {
+public class Bubble extends RangedClass implements SpawnAttack {
     public Bubble(int hp, int atk, int def, int attackRange, float attackSpeed) {
         super(hp, atk, def, attackRange, attackSpeed);
         setName("Bubble");
@@ -12,6 +17,26 @@ public class Bubble extends RangedClass {
     public Bubble() {
         super();
         setName("Bubble");
+    }
+
+    @Override
+    public void attack(float startX, float startY, boolean facingRight, Player p) {
+
+        float dirX = facingRight ? 1 : -1;
+
+        BaseProjectileAttack bubble =
+                new BaseProjectileAttack(
+                        this.atk,
+                        6f,
+                        this.attackRange * 100,
+                        startX,
+                        startY,
+                        dirX,
+                        0,
+                        p
+                );
+
+        spawnProjectile(bubble);
     }
 
     @Override
